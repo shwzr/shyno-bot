f775d382d1647ae5e9f19ae3dd1f1031d1d86007
shwzr <113097122+shwzr@users.noreply.github.com>
2023-01-17 09:23:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({intents})
 const loadCommands = require("./Loaders/loadCommands")
 const loadEvents = require("./Loaders/loadEvents")
 const config = require("./config")
-
+ 
 bot.commands = new Discord.Collection()
 
 bot.login(config.token)
