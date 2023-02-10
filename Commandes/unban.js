77d02a1b67bff383dc5f253d5329d8a6ce6e4acd
shwzr <113097122+shwzr@users.noreply.github.com>
2023-02-10 23:14:41 +0100

    Update unban.js

--- Commandes/unban.js
+++ Commandes/unban.js
@@ -1,36 +1,39 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports = {
-    name: "unban",
-    description: "Débannir un utilisateur",
-    permission: Discord.PermissionFlagsBits.BanMembers,
-    dm: false,
-    options: [
-        {
-            type: "user",
-            name :"utilisateur",
-            description: "L'utilisateur' à débannir",
-            required: true,
-        }, {
-            type: "string",
-            name: "raison",
-            description: "La raison du débanissement",
-            required: false,
-        }
-    ],
-    async run(bot, message, args) {
-        try{
-            let user = args.getUser("utilisateur")
-            if(!user) return message.reply("Pas d'utilisateur !")
-            let reason = args.getString("reason")
-            if(!reason) reason = "Pas de raison fournie."
-            if(!(await message.guild.bans.fetch()).get(user.id)) return message.reply("Cet utilisateur n'est pas banni !")
-            try {await user.send(`Tu as été unban du serveur par ${message.user.tag} pour la raison : \`${reason}\``)} catch(err) {}
-            await message.reply(`${message.user} a unban ${user.tag} pour la raison : \`${reason}\``)
-            await message.guild.members.unban(user, reason) 
-            
-        } catch(err) {
-            return message.reply("Pas d'utilisateur !")
-        }
+  name: "unban",
+  description: "Débannir un utilisateur",
+  permission: Discord.PermissionFlagsBits.BanMembers,
+  dm: false,
+  options: [
+    {
+      type: "user",
+      name: "utilisateur",
+      description: "L'utilisateur à débannir",
+      required: true
+    },
+    {
+      type: "string",
+      name: "raison",
+      description: "La raison du débanissement",
+      required: false
     }
-}
+  ],
+  async run(bot, message, args) {
+    try {
+      let user = args.getUser("utilisateur");
+      if (!user) return message.reply("Pas d'utilisateur !");
+      let reason = args.getString("raison");
+      if (!reason) reason = "Pas de raison fournie.";
+      if (!(await message.guild.bans.fetch()).get(user.id))
+        return message.reply("Cet utilisateur n'est pas banni !");
+      try {
+        await user.send(`Tu as été unban du serveur par ${message.user.tag} pour la raison : \`${reason}\``);
+      } 
+      catch (err) {} await message.reply(`${message.user} a unban ${user.tag} pour la raison : \`${reason}\``);
+      await message.guild.members.unban(user, reason);
+    } catch (err) {
+      return message.reply("Pas d'utilisateur !");
+    }
+  }
+};
